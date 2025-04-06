f40ec5ecb695647fac75879b8ebb33f44081bef6
Campbell <rafaelcampbell@id.uff.br>
2025-04-06 09:09:50 -0300

    Adding configs

--- configs/configs.py
+++ configs/configs.py
@@ -1,9 +1,24 @@
 
-mysql_connection_config = { "host": "localhost", 
+mysql_connection_config = {
+                            "host": "localhost", 
                             "user": "lsiagetrans", 
                             "password": "123abc", 
                             "database": "siagetrans",
                             "auth_plugin": "mysql_native_password"
                           }
 
+mqtt_broker_default_client_config = { 
+                        "host": "localhost",
+                        "port": 1883,
+                        "keepalive": 60,
+                        "subscriber_topic": None,
+                        "publisher_topic": None
+                     }
 
+mqtt_broker_trafficlight_client_config = { 
+                        "host": "localhost",
+                        "port": 1883,
+                        "keepalive": 60,
+                        "subscriber_topic": "messages",
+                        "publisher_topic": None
+                     }
