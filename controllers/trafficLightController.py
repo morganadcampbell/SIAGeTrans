7435975e0be645a841f671452d47539a424f55c0
morganadcampbell <rafaelcampbell@id.uff.br>
2025-11-16 04:04:33 -0300

    Updating TrafficLighttController

--- controllers/trafficLightController.py
+++ controllers/trafficLightController.py
@@ -1,41 +1,42 @@
+from DatabaseController import DatabaseController
 from models.databaseObject import DatabaseObject
 from models.client import Client
 import configs.configs as configs
 import time
-import pickle
 
 class IntersectionClient(Client):
     __controller = None
     def __init__(self, controller):
         clientConfigs = configs.mqtt_broker_intersection_client_config
-        clientConfigs['client_name'] = clientConfigs.get('client_name') + '_' + str(controller.intersectionId)
-        clientConfigs['subscriber_topic'] = '/' + controller.region + clientConfigs.get('subscriber_topic')
+        clientConfigs['client_name'] = clientConfigs.get('client_name') + '_' + str(controller.intersectionId) # subscribe as 'intersection_<id>'
+        clientConfigs['subscriber_topic'] = '/' + controller.region + clientConfigs.get('subscriber_topic') # subscribe to topic '/<region code>/timeUpdates'
         super().__init__(**clientConfigs)
         IntersectionClient.__controller = controller
 
     def messageHandler(client, userdata, message):
         Client.messageHandler(client, userdata, message)
-        payload = message.payload.decode('ascii').split('/')
-        managedTrafficLights = IntersectionClient.__controller.getManagedTrafficLightsId()
-        if payload[0] in managedTrafficLights:
-            IntersectionClient.__controller.updateTimes(payload[0], tuple(payload[1:]))
+        payload = message.payload.decode('ascii').split('/') # receives messages as '<intersectionId>/<phase1_Id>/<phase1_GreenTime>/.../<phaseN_Id>/<phaseN_GreenTime>'
+        intersectionId = payload.pop(0)
+        if intersectionId == IntersectionClient.__controller.intersectionId and all(id in IntersectionClient.__controller.getPhasesId() for id in payload[::2]):
+            IntersectionClient.__controller.updateTimes(dict(zip(payload[::2], payload[1::2])))
 
 class TrafficLightController:
-    def __init__(self, intersectionId : int, region : str, managedTrafficLights : list[DatabaseObject]):
+    def __init__(self, intersectionId : int, region : str, phases : list[DatabaseObject]):
         self.intersectionId = intersectionId
+        self.databaseController = DatabaseController()
         self.region = region
-        self.__managedTrafficLights = {str(x.getId()) : x for x in managedTrafficLights}
+        self.__phases = {str(x.getId()) : x for x in phases}
         self.__active = False
 
-    def getManagedTrafficLightsId(self) -> list[str]: return self.__managedTrafficLights.keys()
+    def getPhasesId(self) -> list[str]: return self.__phases.keys()
 
-    def updateTimes(self, id : str, newTimes : tuple[int]):
-        trafficlight = self.__managedTrafficLights.get(id)
-        print(f"\nUpdating trafficLight_{id} as:\n\tGreenTime: {trafficlight.nr_greentime_s} -> {newTimes[0]}\n\tYellowTime: {trafficlight.nr_yellowtime_s} -> {newTimes[1]}\n\tRedTime: {trafficlight.nr_redtime_s} -> {newTimes[2]}\n")
-        self.__managedTrafficLights.get(id).nr_greentime_s = newTimes[0]
-        self.__managedTrafficLights.get(id).nr_yellowtime_s = newTimes[1]
-        self.__managedTrafficLights.get(id).nr_redtime_s = newTimes[2]
-
+    def updateTimes(self, phasesNewTimes : dict):
+        print(f"\nUpdating intersection_{self.intersectionId} with green times as:")
+        for id,time in phasesNewTimes.items():
+            print(f"\tPhase_{id} with green times as: {time}")
+            self.__phases[id].nr_green_duration = time # updating phase object
+            self.databaseController.update(self.__phases[id]) # updating database
+        
     def startManagement(self):
         self.__active = True
         trafficLightClient = IntersectionClient(self)
